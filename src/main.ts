b09d32602536524938bd2d1b180a54f6a53af047
caofanghui-longhu <caofanghui_rf@126.com>
2021-06-16 00:22:49 +0800

    update.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { AppFactory } from '@/utils/appFactory'
 import App from '@/App.vue'
 import router from '@/router'
 
+// 共用css样式
+import 'normalize.css' // 引用基础兼容性css，normalize.css；文档：https://github.com/necolas/normalize.css
+
 const app = AppFactory(App)
 
 // 注入vue-router，并挂再到app
